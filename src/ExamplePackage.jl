94f401669eac7df143c3ac76a9995c056e2c450c
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-07-22 15:42:51 -0300

    Add imaginary function

--- src/ExamplePackage.jl
+++ src/ExamplePackage.jl
@@ -9,7 +9,16 @@ function double(x)
     return 2x
 end
 
+"""
+    imaginary(a, b)
+
+Return imaginary number given `a` and `b`.
+"""
+function imaginary(a, b)
+    return a + b * im
+end
+
 # public api
-export double
+export double, imaginary
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,6 @@ using Test
 @testset "ExamplePackage.jl" begin
     @test double(2) == 4
     @test double(-1) == -2
+    @test imaginary(1, 2) == 1 + 2 * im
+    @test imaginary(1, -1) == 1 + -1 * im
 end
